38fd176f49366f5f25c87ba6c731897bc8cec71e
Rafal Adamczyk <rafal.adamczyk@grantadesign.com>
2017-04-09 00:40:16 +0200

    simulated annealing

--- Hetmany/Program.cs
+++ Hetmany/Program.cs
@@ -6,7 +6,6 @@ namespace Hetmany
 {
     class Program
     {
-
         private static readonly Random Rng = new Random();
 
         static void Main(string[] args)
@@ -15,26 +14,95 @@ namespace Hetmany
             string userN = args.Length > 0 ? args[0] : null;
             if (!int.TryParse(userN, out n))
             {
-                n = 8; // domyslnie szachownica 8x8
+                n = 10; // domyslnie szachownica 8x8
             }
 
-            int fitness = int.MaxValue;
-            int[] solution = RandomSolution(n);
-            int i = 1;
-            while (fitness > 0 || i > 1000000)
+            double temp;
+            string userTemp = args.Length > 1 ? args[1] : null;
+            if (!double.TryParse(userTemp, out temp))
             {
-                Console.WriteLine("Iteracja {0}", i);
-                solution = RandomSolution(n);
-                fitness = Fitness(solution);
-                i++;
+                temp = 1000; // domyslnie aż 1000 stopniów !!!
             }
 
-            RysujMnieTuSolucje(solution);
+            double coolingRate;
+            string userCoolingRate = args.Length > 2 ? args[2] : null;
+            if (!double.TryParse(userCoolingRate, out coolingRate))
+            {
+                coolingRate = 0.0001; 
+            }
+
+            RysujMnieTuSolucje(SimulatedAnnealing(n, temp, coolingRate));
             Console.ReadLine();
+        }
+
+        private static int[] SimulatedAnnealing(int n, double temp0, double coolingRate)
+        {
+            var temp = temp0;
+
+            int[] currentSolution = RandomSolution(n);
+            int[] bestSolution = currentSolution;
+
+            int currentFitness = Fitness(currentSolution);
+            int bestFitness = Fitness(currentSolution);
+
+            int iteracja = 1;
+            int bestIteracja = iteracja;
+
+            while (temp > 1 && bestFitness > 0)
+            {
+                Console.WriteLine("temp = {0}", temp);
+                int[] newSolution = ModifySolution(currentSolution);
+                int newFitness = Fitness(newSolution);
+                
+                if (newFitness < currentFitness)
+                {
+                    currentSolution = newSolution;
+                    currentFitness = newFitness;
+                    if (currentFitness < bestFitness)
+                    {
+                        bestFitness = currentFitness;
+                        bestSolution = currentSolution;
+                        bestIteracja = iteracja;
+                    }
+                }
+                else
+                {
+                    var random = Rng.Next(1);
+                    if (random < Math.Exp((newFitness - currentFitness) / temp))
+                    {
+                        currentSolution = newSolution;
+                        currentFitness = newFitness;
+                    }
+                }
+
+                temp = temp * (1 - coolingRate);
+                iteracja++;
+            }
+
+            Console.WriteLine("Najlepsza rozwiazanie bylo w iteracji {0}/{1}", bestIteracja, --iteracja);
+            return bestSolution;
+        }
+
+        private static int[] ModifySolution(int[] solution)
+        {
+            int n = solution.Length;
+            int position1 = Rng.Next(n);
+            int position2 = Rng.Next(n);
+
+            int[] newSolution = new int[n];
+            solution.CopyTo(newSolution, 0);
+
+            newSolution[position1] = solution[position2];
+            newSolution[position2] = solution[position1];
+
+            return newSolution;
         }
 
         private static void RysujMnieTuSolucje(int[] solution)
         {
+            Console.WriteLine();
+            Console.WriteLine("Ostateczny fitness = {0}", Fitness(solution));
+            Console.WriteLine();
             for (int i = 0; i < solution.Length; i++)
             {
                 for (int j = 0; j < solution.Length; j++)
